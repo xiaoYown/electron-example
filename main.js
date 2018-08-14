b3aec7e417a90bd680158372b1b84cecd1f47c70
xiaoYown <511687372@qq.com>
2018-08-14 09:51:31 +0800

    no message

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const { app, Menu, BrowserWindow } = require('electron')
 
 const path = require('path')
 const url = require('url')
 
+const menuOptions = require('./src/menu')()
+const { findReopenMenuItem } = require('./src/menu/utils')
+
 const Message = require('./src/handlers/message')
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let winMain // 主视口
 
-function createWindow () {
+function ready () {
+  console.log('ready')
+  let mainMenu = Menu.buildFromTemplate(menuOptions)
+  Menu.setApplicationMenu(mainMenu)
+  
   // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: '100%',
-    height: '100%',
+  winMain = new BrowserWindow({
+    width: 1000,
+    height: 800,
     // resizable: false
   })
+  winMain.setMenu(mainMenu)
 
-  // mainWindow.setMenu(null)
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
+  winMain.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
@@ -35,36 +39,84 @@ function createWindow () {
   this.message.bindShowMessage()
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // winMain.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  winMain.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    winMain = null
   })
+
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+// app.on('ready', ready)
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+// // Quit when all windows are closed.
+// app.on('window-all-closed', function () {
+//   // On OS X it is common for applications and their menu bar
+//   // to stay active until the user quits explicitly with Cmd + Q
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
+
+
+/* 周期事件 */
+app.on('browser-window-created', function () {
+  let reopenMenuItem = findReopenMenuItem()
+  if (reopenMenuItem) reopenMenuItem.enabled = false
 })
+function willFinishLaunching (event) {
+  console.log('will-finish-launching')
+}
+function windowAllClosed (event) {
+  console.log('window-all-closed')
+  let reopenMenuItem = findReopenMenuItem()
+  if (reopenMenuItem) reopenMenuItem.enabled = true
+  app.quit()
+}
+function beforeQuit (event) {
+  console.log('before-quit')
+}
+function willQuit (event) {
+  console.log('will-quit')
+}
+function quit (event) {
+  console.log('quit')
+}
+// 1.当应用程序完成基础的启动的时候被触发
+app.on('will-finish-launching', willFinishLaunching)
+// 2.Electron 完成初始化时被触发
+app.on('ready', ready)
+// 3.当所有的窗口都被关闭时触发
+app.on('window-all-closed', windowAllClosed)
+// 4.在应用程序开始关闭窗口之前触发
+app.on('before-quit', beforeQuit)
+// 5.当所有窗口都已关闭并且应用程序将退出时触发
+app.on('will-quit', willQuit)
+// 6.在应用程序退出时触发
+app.on('quit', quit)
+
+/* 操作事件 */
+function openFile (event) {
+  console.log('open-file')
+  console.log(event)
+}
+// 1.当用户想要在应用中打开一个文件时发出, 也会在一个文件被拖到 dock 并且还没有运行的时候发出
+app.on('open-file', openFile)
+
+
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
+  if (winMain === null) {
+    ready()
   }
 })
 
---
+++ src/menu/utils.js
@@ -0,0 +1,22 @@
+const { Menu } = require('electron')
+
+function findReopenMenuItem () {
+  const menu = Menu.getApplicationMenu()
+  if (!menu) return
+
+  let reopenMenuItem
+  menu.items.forEach(function (item) {
+    if (item.submenu) {
+      item.submenu.items.forEach(function (item) {
+        if (item.key === 'reopenMenuItem') {
+          reopenMenuItem = item
+        }
+      })
+    }
+  })
+  return reopenMenuItem
+}
+
+module.exports = {
+  findReopenMenuItem
+}
